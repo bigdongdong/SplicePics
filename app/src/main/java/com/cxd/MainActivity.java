f9757f67528365eae3a6e3d2992981b18e8bbb28
chenxiaodong <xiaodong.chen@avatr.com>
2024-07-08 10:55:50 +0800

    init for SplicePics

--- app/src/main/java/com/cxd/MainActivity.java
+++ app/src/main/java/com/cxd/MainActivity.java
@@ -1,8 +1,12 @@
 package com.cxd;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,5 +20,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView((mBinding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
+
+        // Registers a photo picker activity launcher in single-select mode.
+        ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
+                registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+                    // Callback is invoked after the user selects a media item or closes the
+                    // photo picker.
+                    if (uri != null) {
+                        Log.d("PhotoPicker", "Selected URI: " + uri);
+                    } else {
+                        Log.d("PhotoPicker", "No media selected");
+                    }
+                });
+
+// Include only one of the following calls to launch(), depending on the types
+// of media that you want to let the user choose from.
+
+// Launch the photo picker and let the user choose images and videos.
+        pickMedia.launch(new PickVisualMediaRequest.Builder()
+                .setMediaType(ActivityResultContracts.PickVisualMedia.ImageAndVideo.INSTANCE)
+                .build());
+
     }
 }
